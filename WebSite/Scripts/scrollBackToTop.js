5c1c4c05199c2ec937bb42c10da3c33781110432
josé Rodrigues <josemiguel443@gmail.com>
2017-07-22 21:49:50 +0100

    new alterations

--- WebSite/Scripts/scrollBackToTop.js
+++ WebSite/Scripts/scrollBackToTop.js
@@ -11,5 +11,6 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
+	document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+	document.documentElement.scrollTop = 0; // For IE and Firefox
+}
